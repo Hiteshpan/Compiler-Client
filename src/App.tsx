911ca7153803642626ef510906e9b485f6b07b95
Hiteshpan <hiteshrtk999@gmail.com>
2024-08-15 23:31:44 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl text-center">Hello World</h1>
+      <Button>Hello Button</Button>
+    </>
+  )
+}
+
+export default App

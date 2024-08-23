7cf8802e1bff86b836c7d617b4396538f7e4f449
Hiteshpan <hiteshrtk999@gmail.com>
2024-08-23 23:01:01 +0530

    environment variable bug fixing

--- src/redux/slices/api.ts
+++ src/redux/slices/api.ts
@@ -9,7 +9,8 @@ import {
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://codesoniyt.onrender.com",
+        // baseUrl: "https://codesoniyt.onrender.com",
+        baseUrl: import.meta.env.VITE_SERVER_URL,
         credentials: "include",
     }),
     tagTypes: ["myCodes", "allCodes"],
